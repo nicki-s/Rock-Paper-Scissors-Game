83e5ce9fe4e070bae99847c2048563b2a99d487d
Nichole Schuldt <nickischuldt@gmail.com>
2022-07-22 15:44:17 -0500

    Add game() function

--- javascript.js
+++ javascript.js
@@ -1,28 +1,58 @@
 function getComputerChoice() {
-    min = Math.ceil(1);
-    max = Math.floor(4);
-    randomNum = Math.floor(Math.random()*(max-min)) + min;
+    const min = Math.ceil(1);
+    const max = Math.floor(4);
+    let randomNum = Math.floor(Math.random()*(max-min)) + min;
 
     if (randomNum == 1) {
-        return "Rock";
+        return "rock";
     }
     else if (randomNum == 2) {
-        return "Paper";
+        return "paper";
     }
     else if (randomNum == 3) {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        return "It's a Tie!";
+        return tie;
+    }
+    else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors" || 
+             playerSelection.toLowerCase() == "paper" && computerSelection == "rock" || 
+             playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+        return playerWins;
     }
     else {
-        return "Error";
+        return computerWins;
     }
 }
 
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let results = playRound(playerSelection, computerSelection);
+        if (results == playerWins) {
+            playerScore++;
+        }
+        else if (results == computerWins) {
+            computerScore++;
+        }
+        else {
+            console.log("No one wins this round!");
+        }
+        console.log("Current Scores: Player: " + playerScore + " Computer: " + computerScore);
+    }
+}
+
+const tie = "It's a Tie!";
+const playerWins = "You Win This Round!";
+const computerWins = "Computer Wins This Round!";
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+console.log("Welcome to the game of Rock Paper Scissors!");
+game();
+
+
