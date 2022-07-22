5492d842d78b00e80bf6a7e99ee2ed1d00b4bc7d
Nichole Schuldt <nickischuldt@gmail.com>
2022-07-22 17:29:03 -0500

    Edit game() function, playRound() function, and main() function

--- javascript.js
+++ javascript.js
@@ -15,12 +15,12 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    if (playerSelection == computerSelection) {
         return tie;
     }
-    else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors" || 
-             playerSelection.toLowerCase() == "paper" && computerSelection == "rock" || 
-             playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+    else if (playerSelection == "rock" && computerSelection == "scissors" || 
+             playerSelection == "paper" && computerSelection == "rock" || 
+             playerSelection == "scissors" && computerSelection == "paper") {
         return playerWins;
     }
     else {
@@ -32,27 +32,42 @@ function game() {
     let playerScore = 0;
     let computerScore = 0;
     for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Please enter your selection: ").toLowerCase();
+        console.log("Your selection: " + playerSelection);
+        let computerSelection = getComputerChoice();
+        console.log("Computer's selection: " + computerSelection);
         let results = playRound(playerSelection, computerSelection);
         if (results == playerWins) {
+            console.log(playerWins);
             playerScore++;
         }
         else if (results == computerWins) {
+            console.log(computerWins);
             computerScore++;
         }
         else {
+            console.log(tie);
             console.log("No one wins this round!");
         }
         console.log("Current Scores: Player: " + playerScore + " Computer: " + computerScore);
+    }
+    if (computerScore > playerScore) {
+        console.log("Computer Wins the Game with a score of " + computerScore + "!");
+    }
+    else if (computerScore < playerScore) {
+        console.log("You win the game with a score of " + playerScore + "!");
+    }
+    else {
+        console.log("It's a tie! No one wins the game.");
     }
 }
 
 const tie = "It's a Tie!";
 const playerWins = "You Win This Round!";
 const computerWins = "Computer Wins This Round!";
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
 
 console.log("Welcome to the game of Rock Paper Scissors!");
+
 game();
 
 
